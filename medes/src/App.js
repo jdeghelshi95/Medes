389c5efc4d8e58ea67af0fddc84a58142ea1ec55
jdeghelshi95 <98611117+jdeghelshi95@users.noreply.github.com>
2022-10-23 00:21:17 -0400

    got all pages setup and tested layouts and fixed error page mis sizing

--- medes/src/App.js
+++ medes/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Sermons from "./pages/Sermons";
+import About from "./pages/About";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-full  ">
+      <div className="">
+        <Nav />
+
+    <div className="">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/sermons" element={<Sermons />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </div>
+
+        <div className=" object-bottom">
+          <Footer className="absolute object-bottom bg-black " />
+        </div>
+      </div>
     </div>
   );
 }
